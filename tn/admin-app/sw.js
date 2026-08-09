a123efcdc5fa3c4072d566e1457fc29e6c3ae73d
adrian271187-a11y <adrian271187@gmail.com>
2026-08-09 10:59:29 -0600

    Update sw.js

--- tn/admin-app/sw.js
+++ tn/admin-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tn-admin-v3';
+const CACHE = 'tn-admin-v4';
 self.addEventListener('install', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.skipWaiting()));
 });
